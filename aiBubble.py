f1bbb3d48b9da40a8a2acec21032d308ff923bf8
Andre-Silva-Peixoto <andrespx2403@gmail.com>
2023-06-05 20:02:56 -0300

    Troca do streamlit pelo flask para utilizar html e css

--- aiBubble.py
+++ aiBubble.py
@@ -1,21 +1,57 @@
-import streamlit as st
+from flask import Flask, request, render_template
 import openai
 import PyPDF2
 import datetime
 import os
 
+app = Flask(__name__)
 
-# FUNÇÃO QUE LÊ O PDF
+# API CHAT GPT
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+
+@app.route("/")
+def page_home():
+    return render_template("index.html")
+
+
+@app.route("/pergunta", methods=["GET", "POST"])
+def page_pergunta():
+    if request.method == "GET":
+        return render_template("pergunta.html")
+    elif request.method == "POST":
+        pergunta = request.form.get("pergunta")
+        uploaded_files = request.files.getlist("pdf_files")
+
+        if pergunta and uploaded_files:
+            # Tamanho da pergunta
+            pergunta = pergunta[:50]
+
+            # Concatena o texto de todos os arquivos PDF
+            texto_pdf = ""
+            for uploaded_file in uploaded_files:
+                texto_pdf += ler_pdf(uploaded_file) + "\n"
+
+            # Inicializa o contexto com o texto dos PDFs e o ano atual
+            contexto = texto_pdf + "Ano atual: " + str(datetime.datetime.now())
+
+            # Diminui o tamanho do contexto para ser mais eficiente o uso de tokens
+            max_tokens_contexto = 4096 - len(pergunta) - len("\nContexto: ")
+            contexto = contexto[:max_tokens_contexto]
+
+            # Obtém a resposta inicial com base no contexto otimizado
+            resposta = obter_resposta(pergunta, contexto)
+
+            # Exibe a resposta
+            return render_template("resposta.html", resposta=resposta)
+
+
 def ler_pdf(file):
     pdf_reader = PyPDF2.PdfReader(file)
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
     return text
-
-
-# API CHAT GPT
-openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 def obter_resposta(pergunta, contexto):
@@ -41,42 +77,5 @@ def obter_resposta(pergunta, contexto):
             return resposta_texto  # Retorna a resposta original caso não encontre um ponto final
 
 
-def main():
-    st.title("Projeto AÍ/Bubble")
-    st.sidebar.title("Projeto IA")
-    st.sidebar.write("André Silva Peixoto")
-    st.sidebar.write("Felipe Angeli Cardoso de Aguiar")
-
-    # Carrega os arquivos PDF
-    uploaded_files = st.file_uploader("Carregue os seus arquivos PDF", type="pdf", accept_multiple_files=True)
-
-    if uploaded_files is not None:
-        # Pergunta pro gpt
-        pergunta = st.text_input("Faça uma pergunta sobre os PDFs: ")
-
-        if pergunta:
-            # Tamanho da pergunta
-            pergunta = pergunta[:50]
-
-            # Concatena o texto de todos os arquivos PDF
-            texto_pdf = ""
-            for uploaded_file in uploaded_files:
-                texto_pdf += ler_pdf(uploaded_file) + "\n"
-
-            # Inicializa o contexto com o texto dos PDFs e o ano atual
-            contexto = texto_pdf + "Ano atual: " + str(datetime.datetime.now())
-
-            # Diminui o tamanho do contexto para ser mais eficiente o uso de tokens
-            max_tokens_contexto = 4096 - len(pergunta) - len("\nContexto: ")
-            contexto = contexto[:max_tokens_contexto]
-
-            # Obtém a resposta inicial com base no contexto otimizado
-            resposta = obter_resposta(pergunta, contexto)
-
-            # Exibe a resposta
-            st.write("Resposta do Chatbot: ")
-            st.write(resposta)
-
-
 if __name__ == "__main__":
-    main()
+    app.run()
